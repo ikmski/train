dee9f0de2d6ea8c8a0a549a1692791b22921c5ab
Masaki Ikeda <ikeda.masaki@tecotec.co.jp>
2014-06-06 21:19:13 +0900

    円周率算出クラスファイル作成

---
+++ pi/pi.h
@@ -0,0 +1,32 @@
+/**
+ * @file  Pi.h
+ * @brief 円周率算出
+ */
+
+#ifndef _PI_H_
+#define _PI_H_
+
+#include <stdio.h>
+
+/*
+ * @class Pi
+ * @brief 円周率算出クラス
+ */
+class Pi
+{
+private:
+
+public:
+
+    Pi();              //!< コンストラクタ
+    virtual ~Pi();     //!< デストラクタ
+
+};
+
+#endif  // _PI_H_
+
+
+
+
+
+
