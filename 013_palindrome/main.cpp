966c10ea684fdff816a1884c84477b66158b3bed
Masaki Ikeda <ikeda.masaki@tecotec.co.jp>
2014-08-29 18:42:30 +0900

    回分数検索プログラム　long型まで拡張

--- 013_palindrome/main.cpp
+++ 013_palindrome/main.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
-#include <math.h>
 #include <string.h>
+#include <limits.h>
 
 void swap(const char* const src, char* const dst)
 {
@@ -15,9 +15,9 @@ void swap(const char* const src, char* const dst)
     //printf("%s <=> %s\n", src, dst);
 }
 
-void dec2bin(const int src, char* const dst)
+void dec2bin(const unsigned long src, char* const dst)
 {
-    int tmp = src;
+    unsigned long tmp = src;
     int digit = 0;
     while (tmp > 0) {
         tmp >>= 1;
@@ -32,27 +32,32 @@ void dec2bin(const int src, char* const dst)
     dst[digit] = '\0';
 }
 
-int calc(int findNum=1)
+unsigned long calc(int findNum=1)
 {
-    int result = 0;
+    unsigned long result = 0;
 
-    char dec[256];
-    char decSwap[256];
-    char bin[256];
-    char binSwap[256];
-    char oct[256];
-    char octSwap[256];
+    char dec[128];
+    char decSwap[128];
+    char bin[128];
+    char binSwap[128];
+    char oct[128];
+    char octSwap[128];
 
     int cnt = 0;
-    int i = 9;
+    unsigned long i = 9;
     while (1) {
         ++i;
+        if (i == ULONG_MAX) break;
 
-        sprintf(dec, "%d", i);
+        if (i%1000000 == 0) {
+            printf("i = %d\n",i);
+        }
+
+        sprintf(dec, "%lu", i);
         swap(dec, decSwap);
         if (0 != strcmp(dec, decSwap)) continue;
 
-        sprintf(oct, "%o", i);
+        sprintf(oct, "%lo", i);
         swap(oct, octSwap);
         if (0 != strcmp(oct, octSwap)) continue;
 
@@ -61,7 +66,7 @@ int calc(int findNum=1)
         if (0 != strcmp(bin, binSwap)) continue;
 
         result = i;
-        //printf("%s, %s, %s\n", dec, oct, bin);
+        printf("%s, %s, %s\n", dec, oct, bin);
         ++cnt;
         if (cnt >= findNum) {
             break;
@@ -79,7 +84,7 @@ int main(int argc, char* argv[])
     // 時間計測開始
     gettimeofday(&startTime, NULL);
 
-    int result =  calc();
+    unsigned long result =  calc(1);
 
     // 時間計測終了
     gettimeofday(&endTime, NULL);
@@ -87,7 +92,7 @@ int main(int argc, char* argv[])
     double usecDiff = static_cast<double>(endTime.tv_usec - startTime.tv_usec);
     timeDiff += (usecDiff/1000000.0);
 
-    printf("%d  %.3f[msec]\n", result, timeDiff*1000.0);
+    printf("%lu  %.3f[msec]\n", result, timeDiff*1000.0);
 
     return 0;
 }
